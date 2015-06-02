824415ab2ce005933e194923e189f340aa72d6c8
Michael Wagner <wagnerdmike@gmail.com>
2015-06-02 07:41:29 -0700

    Add pokedex to list.

--- app/assets/javascripts/pokedex-1A.js
+++ app/assets/javascripts/pokedex-1A.js
@@ -1,5 +1,13 @@
 Pokedex.RootView.prototype.addPokemonToList = function (pokemon) {
+  var $pokemon = $('<li>').addClass('poke-list-item')
+    .attr('name', pokemon.name).attr('poke_type', pokemon.poke_type)
+    .text(pokemon.attributes.name + ' : ' + pokemon.attributes.poke_type);
+  this.$pokeList.append($pokemon);
+  this.pokes.create(pokemon.toJSON());
 };
 
 Pokedex.RootView.prototype.refreshPokemon = function () {
+  for (var i = 0; i < this.pokes.length; i++) {
+    this.addPokemonToList(this.pokes[i]);
+  }
 };
